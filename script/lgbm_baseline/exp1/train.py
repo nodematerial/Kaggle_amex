711a04aef3dff29d1b275518f638ae5f326e09a4
kota <kouta.tus1@gmail.com>
2022-06-14 10:53:46 +0900

    add logger to train.py

--- script/lgbm_baseline/exp1/train.py
+++ script/lgbm_baseline/exp1/train.py
@@ -1,3 +1,4 @@
+from random import triangular
 import numpy as np
 import pandas as pd
 import datetime
@@ -13,6 +14,18 @@ import lightgbm as lgb
 import optuna
 
 warnings.filterwarnings('ignore')
+
+def init_logger(log_file='train.log'):
+    from logging import getLogger, INFO, FileHandler,  Formatter,  StreamHandler
+    logger = getLogger(__name__)
+    logger.setLevel(INFO)
+    handler1 = StreamHandler()
+    handler1.setFormatter(Formatter("%(message)s"))
+    handler2 = FileHandler(filename=log_file)
+    handler2.setFormatter(Formatter("%(message)s"))
+    logger.addHandler(handler1)
+    logger.addHandler(handler2)
+    return logger
 
 def amex_metric(y_true: np.array, y_pred: np.array) -> float:
 
@@ -49,7 +62,7 @@ def custom_accuracy(preds, data):
     acc = amex_metric(y_true, preds)
     return ('custom_accuracy', acc, True)
 
-def create_train(CFG):
+def create_train(CFG, LOGGER = None):
     df = pd.read_feather(f'../../../data/raw/train_data.ftr')
     cid = pd.Categorical(df.pop('customer_ID'), ordered=True)
     last = (cid != np.roll(cid, -1)) # mask for last statement of every customer
@@ -69,35 +82,34 @@ def create_train(CFG):
     df = df.loc[last, feature_last].rename(columns={f: f"{f}_last" for f in feature_last}).set_index(np.asarray(cid[last]))
     print('Computed last')
 
-    df = pd.concat([df, df_max, df_avg], axis=1)
-    train = df
-    print(f"train shape: {train.shape}")
-
+    train = pd.concat([df, df_max, df_avg], axis=1)
     del df, df_avg, df_max, cid, last
     gc.collect()
 
     target = pd.read_csv('../../../data/raw/train_labels.csv').target.values
-    print(f"target shape: {target.shape}")
 
     features = [f for f in train.columns if f != 'customer_ID' and f != 'target']
-    print(f"{len(features)} features")
+    if LOGGER:
+        LOGGER.info(f"{len(features)} features")
 
     return train, target, features
 
-def tuning(train, target, features, CFG):
+def tuning(train, target, features, CFG, LOGGER = None):
+    LOGGER.info('[Optuna parameter tuning]')
+    kf = StratifiedKFold(n_splits=5)
 
     def objective(trial):
         score_list = []
-        kf = StratifiedKFold(n_splits=5)
-
         start_time = datetime.datetime.now()
+        global trial_number
+        trial_number += 1
+        LOGGER.info(f'[ Trial {trial_number} Start ]')
+
         for fold, (idx_tr, idx_va) in enumerate(kf.split(train, target)):
-            print('[ Trial Start ]')
             train_x = train.iloc[idx_tr][features]
             valid_x = train.iloc[idx_va][features]
             train_y = target[idx_tr]
             valid_y = target[idx_va]
-            
             dtrain = lgb.Dataset(train_x, label=train_y)
             dvalid = lgb.Dataset(valid_x, label=valid_y)
 
@@ -106,7 +118,8 @@ def tuning(train, target, features, CFG):
                 'metric': 'custom',
                 'lambda_l1': trial.suggest_loguniform('lambda_l1', 1e-8, 10.0),
                 'lambda_l2': trial.suggest_loguniform('lambda_l2', 1e-8, 10.0),
-                'num_leaves': trial.suggest_int('num_leaves', 2, 256),
+                'num_leaves': trial.suggest_int('num_leaves', 64, 512),
+                'learning_rate': trial.suggest_uniform('learning_rate', 0.005, 0.1),
                 'feature_fraction': trial.suggest_uniform('feature_fraction', 0.4, 1.0),
                 'bagging_fraction': trial.suggest_uniform('bagging_fraction', 0.4, 1.0),
                 'bagging_freq': trial.suggest_int('bagging_freq', 1, 7),
@@ -115,8 +128,8 @@ def tuning(train, target, features, CFG):
             }
     
             gbm = lgb.train(param, dtrain, valid_sets=[dvalid], 
-                            num_boost_round=250,
-                            early_stopping_rounds=15,
+                            num_boost_round=CFG['num_boost_round'],
+                            early_stopping_rounds=50,
                             verbose_eval=False,
                             feval = [custom_accuracy])
             preds = gbm.predict(valid_x)
@@ -126,17 +139,21 @@ def tuning(train, target, features, CFG):
             if CFG['ONLY_FIRST_FOLD']: break # we only want the first fold
 
         cv_score = sum(score_list) / len(score_list)
-        print(f"{Fore.GREEN}{Style.BRIGHT}CV Score | {str(datetime.datetime.now() - start_time)[-12:-7]} |"
-        f" Score = {cv_score:.5f}{Style.RESET_ALL}")
+        if LOGGER:
+            LOGGER.info(f"CV Score | {str(datetime.datetime.now() - start_time)[-12:-7]} | Score = {cv_score:.5f}")
+        else:
+            print(f"{Fore.GREEN}{Style.BRIGHT}CV Score | {str(datetime.datetime.now() - start_time)[-12:-7]} |"
+                  f" Score = {cv_score:.5f}{Style.RESET_ALL}")
 
         return cv_score
 
     study = optuna.create_study(direction='maximize')
-    study.optimize(objective, n_trials=50)
+    study.optimize(objective, n_trials=CFG['optuna_trial'])
+    best_params = study.best_params
+    LOGGER.info(best_params)
+    return best_params
 
-    return study.best_params
-
-def train_model(train, target, features, best_param, CFG):
+def train_model(train, target, features, best_param, CFG, LOGGER = None):
     score_list = []
     kf = StratifiedKFold(n_splits=5)
     for fold, (idx_tr, idx_va) in enumerate(kf.split(train, target)):
@@ -149,8 +166,8 @@ def train_model(train, target, features, best_param, CFG):
         dvalid = lgb.Dataset(valid_x, label=valid_y)
 
         gbm = lgb.train(best_param, dtrain, valid_sets=[dvalid], 
-                        num_boost_round=250,
-                        early_stopping_rounds=15,
+                        num_boost_round=CFG['num_boost_round'],
+                        early_stopping_rounds=20,
                         callbacks=[lgb.log_evaluation(10)],
                         feval = [custom_accuracy])
 
@@ -159,17 +176,26 @@ def train_model(train, target, features, best_param, CFG):
         score_list.append(score)
         file = CFG['logdir'] + f'/model_fold{fold}.pkl'
         pickle.dump(gbm, open(file, 'wb'))
-
-    print(f"{Fore.GREEN}{Style.BRIGHT}OOF Score: {np.mean(score_list):.5f}{Style.RESET_ALL}")
+    if LOGGER:
+        LOGGER.info(f"OOF Score: {np.mean(score_list):.5f}")
+    else:
+        print(f"{Fore.GREEN}{Style.BRIGHT}OOF Score: {np.mean(score_list):.5f}{Style.RESET_ALL}")
 
 def main():
     with open('config.yml', 'r') as yml:
         CFG = yaml.load(yml, Loader=yaml.SafeLoader)
+
     os.makedirs(CFG['logdir'], exist_ok=True)
+    LOGGER = None
+    if CFG['log'] == True:
+        LOGGER = init_logger(log_file=CFG['logdir']+'/train.log')
+
     train, target, features = create_train(CFG)
-    best_params = tuning(train, target, features, CFG)
+    best_params = tuning(train, target, features, CFG, LOGGER)
     best_params['force_col_wise'] = True
-    train_model(train, target, features, best_params, CFG)
+    train_model(train, target, features, best_params, CFG, LOGGER)
 
 if __name__ == '__main__':
+    # global variable
+    trial_number = 0
     main()
