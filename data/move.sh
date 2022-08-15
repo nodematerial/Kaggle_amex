31c22e9f1be175149e63b3f8668850ca156d1013
kota <kouta.tus1@gmail.com>
2022-08-15 18:29:48 +0900

    add experiment directories

---
+++ data/move.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cd feature
+mkdir all_features
+#mkdir all_features/train
+#mkdir all_features/test
+
+rsync */train/* all_features/train/
+rsync */test/*  all_features/test/
+
+mkdir all_features/train/tmp
+mkdir all_features/test/tmp
+
+cd all_features/train
+
+while read line
+do
+filename="$line.pickle"
+mv $filename tmp
+done < ../../../feature_groups/all_features/importance_top500.txt
+
+cd ../test
+
+while read line
+do
+filename="$line.pickle"
+mv $filename tmp
+done < ../../../feature_groups/all_features/importance_top500.txt
+
+cd ..
+rm train/*
+mv train/tmp/* train/
+rm -r train/tmp
+
+rm test/*
+mv test/tmp/* test/
+rm -r test/tmp
---
+++ script/lgbm_baseline/exp10_n(all_features_dart2)/utils.py
@@ -0,0 +1,69 @@
+import pandas as pd 
+import yaml
+import glob
+import os
+import pickle 
+import lightgbm as lgb
+
+def init_logger(log_file='train.log'):
+    from logging import getLogger, INFO, FileHandler,  Formatter,  StreamHandler
+    logger = getLogger(__name__)
+    logger.setLevel(INFO)
+    handler1 = StreamHandler()
+    handler1.setFormatter(Formatter("%(message)s"))
+    handler2 = FileHandler(filename=log_file)
+    handler2.setFormatter(Formatter("%(message)s"))
+    logger.addHandler(handler1)
+    logger.addHandler(handler2)
+    return logger
+
+
+def set_STDOUT(logger):
+    if logger == None:
+        return print
+    else:
+        return logger.info
+    
+    
+    
+class DartEarlyStopping(object):
+    """DartEarlyStopping"""
+
+    def __init__(self, data_name, monitor_metric, stopping_round):
+        self.data_name = data_name
+        self.monitor_metric = monitor_metric
+        self.stopping_round = stopping_round
+        self.best_score = None
+        self.best_model = None
+        self.best_score_list = []
+        self.best_iter = 0
+
+    def _is_higher_score(self, metric_score, is_higher_better):
+        if self.best_score is None:
+            return True
+        return (self.best_score < metric_score) if is_higher_better else (self.best_score > metric_score)
+
+    def _deepcopy(self, x):
+        # copy.deepcopyではlightgbmのモデルは完全にコピーされないためpickleを使用
+        return pickle.loads(pickle.dumps(x))
+
+    def __call__(self, env):
+        evals = env.evaluation_result_list
+        for data, metric, score, is_higher_better in evals:
+            if data != self.data_name or metric != self.monitor_metric:
+                continue
+            if not self._is_higher_score(score, is_higher_better):
+                if env.iteration - self.best_iter > self.stopping_round:
+                    # 終了させる
+                    eval_result_str = '\t'.join([lgb.callback._format_eval_result(x) for x in self.best_score_list])
+                    lgb.basic._log_info(f"Early stopping, best iteration is:\n[{self.best_iter+1}]\t{eval_result_str}") 
+                    lgb.basic._log_info(f"You can get best model by \"DartEarlyStopping.best_model\"")
+                    raise lgb.callback.EarlyStopException(self.best_iter, self.best_score_list)
+                return
+            # dartでは過去の木も更新されてしまうため、deepcopyしておく
+            self.best_model = self._deepcopy(env.model)
+            self.best_iter = env.iteration
+            self.best_score_list = evals
+            self.best_score = score
+            return
+        raise ValueError("monitoring metric not found")
